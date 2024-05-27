d8fd49218385496ebf56f3c61e383ed5a0010256
kaid220 <xifural75@yandex.ru>
2024-05-27 20:24:21 +0500

    правки jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,17 +1,40 @@
-task_branch="${jenkinsBranch_name}"
-testUserPassword1="${jenkinsTestUserPassword1}"
-testUserPassword2="${jenkinsTestUserPassword2}"
-remote="${isRemote}"
+pipline{
+    agent any
+    parameters{
+        string(name: 'jenkinsBranch_name')
+        string(name: 'jenkinsTestUserPassword1')
+        string(name: 'jenkinsTestUserPassword2')
+        string(name: 'isRemote')
 
-def currentBranch = task_branch.contains("origin")?task_branch.split('/')[1]:task_branch.trim()
-currentBuild.displayName = "$currentBranch"
-
-node{
-    withEnv("branch=$currentBranch"){
-        stage("Build"){
-            echo "Ветка сборки = '$currentBranch', приступаем к клонированию репозитория"
-            git url: 'https://github.com/kaid220/BitrixAT.git'
+    }
+    stages{
+        stage('Checkout to branch'){
+            steps{
+                script {
+                    def currentBranch = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim
+                    def targetBranch = "${params.jenkinsBranch_name}"
+                    echo "Текущая ветка = '$currentBranch'"
+                    echo "Необходимая ветка = '$targetBranch'"
+                    if (currentBranch != targetBranch) git branch: targetBranch, url: "https://github.com/kaid220/BitrixAT.git"
+                }
+            }
+        }
+        stage('LocalSetting Creator'){
+            steps {
+                script {
+                    echo "Запущено создание файла localSetting.properties"
+                    writeFile file: 'localSetting.properties',text:"""
+                    userLogin1 = xifural75@yandex.ru
+                    userPassword1 =${params.jenkinsTestUserPassword1}
+                    userLogin2 = xifural76@yandex.ru
+                    userPassword2 =${params.jenkinsTestUserPassword2}
+                    baseurl=https://b24-wqf2lv.bitrix24.ru
+                    dataBaseUserName=root
+                    dataBaseRootPassword=rootPassword
+                    isRemote=${params.isRemote}"""
+                    sh 'cat localSetting.properties'
+                }
+            }
         }
     }
 }
-
