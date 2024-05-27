a2e3522527841c3750ae28b3d2093ae93d7168bb
kaid220 <xifural75@yandex.ru>
2024-05-27 20:53:59 +0500

    правки jenkins1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,29 +1,28 @@
-pipline{
-    agent any
-    parameters{
-        string(name: 'jenkinsBranch_name')
-        string(name: 'jenkinsTestUserPassword1')
-        string(name: 'jenkinsTestUserPassword2')
-        string(name: 'isRemote')
-
-    }
-    stages{
-        stage('Checkout to branch'){
-            steps{
-                script {
-                    def currentBranch = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim
-                    def targetBranch = "${params.jenkinsBranch_name}"
-                    echo "Текущая ветка = '$currentBranch'"
-                    echo "Необходимая ветка = '$targetBranch'"
-                    if (currentBranch != targetBranch) git branch: targetBranch, url: "https://github.com/kaid220/BitrixAT.git"
-                }
-            }
+pipline {
+        agent any
+        parameters {
+                string(name: 'jenkinsBranch_name')
+                string(name: 'jenkinsTestUserPassword1')
+                string(name: 'jenkinsTestUserPassword2')
+                string(name: 'isRemote')
         }
-        stage('LocalSetting Creator'){
-            steps {
-                script {
-                    echo "Запущено создание файла localSetting.properties"
-                    writeFile file: 'localSetting.properties',text:"""
+        stages {
+                stage('Checkout to branch') {
+                        steps {
+                                script {
+                                        def currentBranch = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim
+                                        def targetBranch = "${params.jenkinsBranch_name}"
+                                        echo "Текущая ветка = '$currentBranch'"
+                                        echo "Необходимая ветка = '$targetBranch'"
+                                        if (currentBranch != targetBranch) git branch: targetBranch, url: "https://github.com/kaid220/BitrixAT.git"
+                                }
+                        }
+                }
+                stage('LocalSetting Creator') {
+                        steps {
+                                script {
+                                        echo "Запущено создание файла localSetting.properties"
+                                        writeFile file: 'localSetting.properties', text: """
                     userLogin1 = xifural75@yandex.ru
                     userPassword1 =${params.jenkinsTestUserPassword1}
                     userLogin2 = xifural76@yandex.ru
@@ -32,9 +31,9 @@ pipline{
                     dataBaseUserName=root
                     dataBaseRootPassword=rootPassword
                     isRemote=${params.isRemote}"""
-                    sh 'cat localSetting.properties'
+                                        sh 'cat localSetting.properties'
+                                }
+                        }
                 }
-            }
         }
-    }
 }
