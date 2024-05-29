5a8cb91275365286b9f5d4053f89054d09332f3f
kaid220 <xifural75@yandex.ru>
2024-05-29 19:08:21 +0500

    попытка1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -35,6 +35,7 @@ pipeline {
                         }
                 }
                 stage('Run failed tests'){
+                        echo currentBuild.result
                         when{
                                 expression { currentBuild.result == 'FAILURE'}
                         }
